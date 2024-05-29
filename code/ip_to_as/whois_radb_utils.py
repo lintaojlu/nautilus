83703d98786ce12a2f3867ec8059326040d8e25b
lintaojlu <lintaojlu@qq.com>
2024-05-29 17:53:41 +0800

    update: use multi-thread to speed up

--- code/ip_to_as/whois_radb_utils.py
+++ code/ip_to_as/whois_radb_utils.py
@@ -5,6 +5,9 @@ import pickle, subprocess, time, sys
 import warnings
 
 from tqdm import tqdm
+import subprocess
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from tqdm import tqdm
 
 warnings.filterwarnings("ignore")
 root_dir = Path(__file__).resolve().parents[2]
@@ -19,6 +22,22 @@ def save_radb_whois_output(radb_output, ip_version=4, tags='default'):
 
     with open(save_file, 'wb') as fp:
         pickle.dump(radb_output, fp)
+
+
+def load_radb_whois_output(args, in_chunks, ips_list=[], tags='default'):
+    file_location = root_dir / 'stats/ip2as_data/radb_whois_output_{}'.format(tags)
+
+    if Path(file_location).exists():
+        with open(file_location, 'rb') as fp:
+            radb_output = pickle.load(fp)
+    else:
+        if len(ips_list) > 0:
+            radb_output = generate_ip2as_for_list_of_ips(ips_list, tags, args, in_chunks=in_chunks)
+        else:
+            print(f'Please enter either valid file tag or ips list')
+            return None
+
+    return radb_output
 
 
 def generate_ip2as_for_list_of_ips(ip_version=4, list_of_ips=[], tags='default', args=None, in_chunks=True):
@@ -44,49 +63,42 @@ def generate_ip2as_for_list_of_ips(ip_version=4, list_of_ips=[], tags='default',
 
     radb_output = {}
 
-    for count, ip in tqdm(enumerate(list_of_ips), desc='RADB whois', total=len(list_of_ips)):
+    with ThreadPoolExecutor(max_workers=2) as executor:
+        future_to_ip = {executor.submit(whois_query, ip, whois_cmd_location): ip for ip in list_of_ips}
+        for count, future in tqdm(enumerate(as_completed(future_to_ip)), desc='RADB whois', total=len(list_of_ips)):
+            ip, list_out = future.result()
+            ip, results = parse_whois_output(ip, list_out)
+            if results:
+                ip_to_as_map_result = radb_output.get(ip, [])
+                ip_to_as_map_result.extend(results)
+                radb_output[ip] = ip_to_as_map_result
 
-        cmd_str = f'{whois_cmd_location} -h whois.radb.net {ip}'
-        proc = subprocess.Popen(cmd_str, stdout=subprocess.PIPE, shell=True)
-        (out, err) = proc.communicate()
-        list_out = out.decode(errors='ignore').split('\n')
-
-        for item in list_out:
-            if 'AS' in item and ('origin:' in item.lower() or 'aut-num:' in item.lower()):
-                try:
-                    result = item.split(":")[1].strip()
-                    if result != '':
-                        ip_to_as_map_result = radb_output.get(ip, [])
-                        ip_to_as_map_result.append(result)
-                        radb_output[ip] = ip_to_as_map_result
-                except:
-                    continue
-                    # print(f'Failed for this item: {item}')
-
-        if count % 1000 == 0:
-            # print(f'Doing a partial save at count {count}')
-            save_radb_whois_output(radb_output, ip_version, tags)
+            if count % 1000 == 0:
+                save_radb_whois_output(radb_output, ip_version, tags)
 
     print(f'Doing a final save with {len(radb_output)} IPs processed')
     save_radb_whois_output(radb_output, ip_version, tags)
-
     return radb_output
 
 
-def load_radb_whois_output(args, in_chunks, ips_list=[], tags='default'):
-    file_location = root_dir / 'stats/ip2as_data/radb_whois_output_{}'.format(tags)
+def whois_query(ip, whois_cmd_location):
+    cmd_str = f'{whois_cmd_location} -h whois.radb.net {ip}'
+    proc = subprocess.Popen(cmd_str, stdout=subprocess.PIPE, shell=True)
+    out, _ = proc.communicate()
+    return ip, out.decode(errors='ignore').split('\n')
 
-    if Path(file_location).exists():
-        with open(file_location, 'rb') as fp:
-            radb_output = pickle.load(fp)
-    else:
-        if len(ips_list) > 0:
-            radb_output = generate_ip2as_for_list_of_ips(ips_list, tags, args, in_chunks=in_chunks)
-        else:
-            print(f'Please enter either valid file tag or ips list')
-            return None
 
-    return radb_output
+def parse_whois_output(ip, list_out):
+    results = []
+    for item in list_out:
+        if 'AS' in item and ('origin:' in item.lower() or 'aut-num:' in item.lower()):
+            try:
+                result = item.split(":")[1].strip()
+                if result != '':
+                    results.append(result)
+            except:
+                continue
+    return ip, results
 
 
 args = {'whois_cmd_location': '/home/lintao/anaconda3/envs/ki3/bin/whois'}
@@ -103,7 +115,17 @@ if __name__ == '__main__':
     else:
         with open(root_dir / f'stats/mapping_outputs/all_ips_v{ip_version}', 'rb') as fp:
             list_of_ips = pickle.load(fp)
-    list_of_ips = ['66.85.82.9', '156.225.182.1', '67.59.254.241', '103.78.227.1', '193.34.197.140', '23.111.226.1', '193.0.214.1', '152.255.147.235', '216.19.218.1']
+    # list_of_ips = ['66.85.82.9', '156.225.182.1', '67.59.254.241', '103.78.227.1', '193.34.197.140', '23.111.226.1',
+    #                '193.0.214.1', '152.255.147.235', '216.19.218.1']
+
+    import random
+
+
+    def generate_random_ipv4():
+        return f"{random.randint(1, 255)}.{random.randint(0, 255)}.{random.randint(0, 255)}.{random.randint(1, 255)}"
+
+
+    list_of_ips = [generate_random_ipv4() for _ in range(100)]
 
     if mode == 0:
         in_chunks = True
