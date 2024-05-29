b9f1245a9ddd48fecbcad75743a1980a24fd05f4
lintaojlu <lintaojlu@qq.com>
2024-05-29 16:16:18 +0800

    update: Use apis instead of selenium

--- code/ip_to_as/whois_rpki_utils.py
+++ code/ip_to_as/whois_rpki_utils.py
@@ -1,23 +1,12 @@
+import datetime
 from pathlib import Path
 
 import pickle, time, sys
+import requests
 
 import warnings
 
-from selenium.webdriver.chrome.service import Service
-
 warnings.filterwarnings("ignore")
-
-from selenium.webdriver.common.keys import Keys
-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-from selenium.webdriver.chrome.options import Options
-
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 root_dir = Path(__file__).resolve().parents[2]
 
@@ -33,88 +22,40 @@ def save_rpki_whois_output(rpki_output, ip_version=4, tags='default'):
         pickle.dump(rpki_output, fp)
 
 
-def generate_ip2as_for_list_of_ips(ip_version=4, list_of_ips=[], tags='default', args=None, in_chunks=True):
+def generate_ip2as_for_list_of_ips(ip_version=4, list_of_ips=[], tags='default', args=None, in_chunks=False):
     rpki_output = {}
-    url = 'https://rpki-validator.ripe.net/'
 
-    chrome_options = Options()
-    capabilities = DesiredCapabilities.CHROME
-    capabilities["pageLoadStrategy"] = "eager"
-    chrome_options.add_argument('--headless')
-    chromedriver_location = args.get('chromedriver_location', None)
-    chrome_service = Service(executable_path=chromedriver_location)
-
-
-    if in_chunks:
-        num_parallel = args.get('num_parallel', 5)
-        max_ips_to_process = args.get('max_ips_to_process', None)
-        chromedriver_binary = args.get('chromedriver_binary', None)
-        chromedriver_location = args.get('chromedriver_location', None)
-
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.binary_location = chromedriver_binary
-
-        all_ips_file = args.get('ips_file_location', None)
-
-        with open(all_ips_file, 'rb') as fp:
-            all_ips = pickle.load(fp)
-
-        all_ips = list(all_ips)
-
-        if num_parallel * max_ips_to_process > len(all_ips):
-            list_of_ips = all_ips[(num_parallel - 1) * max_ips_to_process:]
-        else:
-            list_of_ips = all_ips[(num_parallel - 1) * max_ips_to_process: server_id * max_ips_to_process]
-
-    else:
-        if len(list_of_ips) == 0:
-            print(f'Invalid list passed. Pass valid list of IPs')
-            return None
-
-    try:
-        driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
-        driver.get(url)
-
-        for count, ip_address in enumerate(list_of_ips):
-
-            query_box = driver.find_element(By.CLASS_NAME, 'el-input__inner')
-            query_box.clear()
-            query_box.send_keys(ip_address)
-
-            button = driver.find_element(By.CLASS_NAME, 'el-button--primary')
-            button.click()
-
-            time.sleep(1)
-
-            element = WebDriverWait(driver, 1).until(
-                EC.presence_of_element_located((By.CLASS_NAME, "validation-header")))
-
-            while True:
-                try:
-                    content = driver.find_elements(By.CLASS_NAME, 'mono')
-                    result = [item.text for item in content if 'AS' in item.text]
-                    print(f'IP address is {ip_address} and result is {result}')
-                    break
-                except:
-                    print('Retrying again')
-                    time.sleep(0.5)
-
-            if len(result) > 0:
-                rpki_output[ip_address] = result
-
-            if count % 100 == 0:
-                print(f'Doing a partial save at count {count}')
-                save_rpki_whois_output(rpki_output, ip_version, tags)
-
-        driver.close()
-        driver.quit()
-
-        print(f'Doing a final save with {len(rpki_output)} IPs processed')
-        save_rpki_whois_output(rpki_output, ip_version, tags)
-
-    except Exception as e:
-        print(f'We got an error: {str(e)}')
+    if len(list_of_ips) == 0:
+        print(f'Invalid list passed. Pass valid list of IPs')
         return None
+
+    for count, ip_address in enumerate(list_of_ips):
+        try:
+            url = f'https://rest.bgp-api.net/api/v1/prefix/{ip_address}/32/search'
+            response = requests.get(url)
+            data = response.json()
+            result = data.get('result', {})
+            meta = result.get('meta', [])
+            asns = []
+
+            for entry in meta:
+                if entry.get('sourceType') == 'bgp':
+                    asns.extend(entry.get('originASNs', []))
+
+            if asns:
+                print(f'IP address is {ip_address} and ASNs are {asns}')
+                rpki_output[ip_address] = asns
+            else:
+                print(f'No AS information found for IP {ip_address}')
+        except Exception as e:
+            print(f'Error processing IP {ip_address}: {str(e)}')
+
+        if count % 100 == 0:
+            print(f'Doing a partial save at count {count}')
+            save_rpki_whois_output(rpki_output, ip_version, tags)
+
+    print(f'Doing a final save with {len(rpki_output)} IPs processed')
+    save_rpki_whois_output(rpki_output, ip_version, tags)
 
     return rpki_output
 
@@ -138,12 +79,13 @@ def load_rpki_whois_output(args, in_chunks, ips_list=None, tags='default'):
 
 
 if __name__ == '__main__':
-
+    start_time = datetime.datetime.now()
+    print(start_time)
     mode = 1
     args = {}
-    list_of_ips = []
-
     ip_version = 4
+    list_of_ips = ['66.85.82.9', '156.225.182.1', '67.59.254.241', '103.78.227.1', '193.34.197.140', '23.111.226.1',
+                   '193.0.214.1', '152.255.147.235', '216.19.218.1']
 
     if mode == 0:
         args['num_parallel'] = int(sys.argv[1])
@@ -152,18 +94,14 @@ if __name__ == '__main__':
         args['ips_file_location'] = root_dir / 'stats/mapping_outputs/all_ips_v{}'.format(ip_version)
         args['chromedriver_binary'] = input('Enter chromedriver binary full path: ')
         args['chromedriver_location'] = root_dir / 'chromedriver'
-    else:
-        args['chromedriver_location'] = root_dir / 'chromedriver'
-
-        with open(root_dir / f'stats/mapping_outputs/all_ips_v{ip_version}', 'rb') as fp:
-            list_of_ips = pickle.load(fp)
-    # list_of_ips = ['66.85.82.9', '156.225.182.1', '67.59.254.241', '103.78.227.1', '193.34.197.140', '23.111.226.1', '193.0.214.1', '152.255.147.235', '216.19.218.1']
-
-    if mode == 0:
         in_chunks = True
     else:
+        args['chromedriver_location'] = root_dir / 'chromedriver'
+        # with open(root_dir / f'stats/mapping_outputs/all_ips_v{ip_version}', 'rb') as fp:
+        #     list_of_ips = pickle.load(fp)
         in_chunks = False
 
     rpki_output = generate_ip2as_for_list_of_ips(ip_version, list_of_ips, args=args, in_chunks=in_chunks)
-
+    print(rpki_output)
     print(f'We got results for {len(rpki_output)} IPs')
+    print(f'Time taken: {datetime.datetime.now() - start_time}')
